380d35b8b283297183090fd2313663efcc306dd1
Mark Dodd <mdodd@ducodesign.ca>
2022-12-11 13:02:16 -0700

    Moved testdata into a seperate class.

--- LoopDrawingAcadUI/AcadLoopDrawingTest.cs
+++ LoopDrawingAcadUI/AcadLoopDrawingTest.cs
@@ -15,61 +15,8 @@ namespace LoopDrawingAcadUI
     internal class AcadLoopDrawingTest
     {
     
-        private const string DefaultPathName = @"c:\mark\acadtesting\some_drawing.dwg";
-        private const string TITPathName = @"c:\mark\acadtesting\Loop Drawing Typical - TIT.dwg";
-
-        private Dictionary<string, string> TagDict;
-
-        public AcadLoopDrawingTest()
-        {
-            TagDict = new Dictionary<string, string>()
-                {
-                    { "DESCRIPTION_01", "DESCRIPTION_01"},
-                    {"DESCRIPTION_02", "DESCRIPTION_02"},
-                    {"DESCRIPTION_03", "DESCRIPTION_03"},
-                    {"MANUFACTURER_01", "MANUFACTURER_01"},
-                    {"MODEL_01", "MODEL_01"},
-                    {"RANGE_01", "RANGE_01"},
-                    {"TAG_01", "TAG_01"},
-                    {"TAG_02", "TAG_02"},
-                    {"CON_01", "CON_01"},
-                    {"CON_02", "CON_02"},
-                    {"JB_TAG_01", "JB_TAG_01"},
-                    {"JB_TS_01", "JB_TS_01"},
-                    {"JB_TB_01", "JB_TB_01"},
-                    {"JB_TB_02", "JB_TB_02"},
-                    {"JB_TB_03", "JB_TB_03"},
-                    {"PNL_TS_01", "PNL_TS_01"},
-                    {"PNL_TB_01", "PNL_TB_01"},
-                    {"PNL_TB_02", "PNL_TB_02"},
-                    {"PNL_TB_03", "PNL_TB_03"},
-                    {"WIRE_CLR_01", "WIRE_CLR_01"},
-                    {"WIRE_CLR_02", "WIRE_CLR_02"},
-                    {"WIRE_CLR_03", "WIRE_CLR_03"},
-                    {"WIRE_CLR_04", "WIRE_CLR_04"},
-                    {"WIRE_CLR_05", "WIRE_CLR_05"},
-                    {"WIRE_CLR_06", "WIRE_CLR_06"},
-                    {"WIRE_CLR_07", "WIRE_CLR_07"},
-                    {"WIRE_CLR_08", "WIRE_CLR_08"},
-                    {"PAIR_01", "PAIR_01"},
-                    {"PAIR_02", "PAIR_02"},
-                    {"WIRE_TAG_01", "WIRE_TAG_01"},
-                    {"WIRE_TAG_02", "WIRE_TAG_02"},
-                    {"WIRE_TAG_03", "WIRE_TAG_03"},
-                    {"CABLE_TAG_01", "CABLE_TAG_01"},
-                    {"CABLE_TAG_02", "CABLE_TAG_02"},
-                    {"BREAKER_01", "BREAKER_01"},
-                    {"MOD_TERM_01", "MOD_TERM_01"},
-                    {"FUNCTIONAL_ID_01", "FUNCTIONAL_ID_01"},
-                    {"LOOP_NO_01", "LOOP_NO_01"},
-                    {"HIGH_ALARM_01", "HIGH_ALARM_01"},
-                    {"LOW_ALARM_01", "LOW_ALARM_01"},
-                    {"RACK_01", "RACK_01"},
-                    {"SLOT_01", "SLOT_01"},
-                    {"CHANNEL_01", "CHANNEL_01"},
-                    {"DRAWING_NO_01", "DRAWING_NO_01"},
-                };
-        }
+        private const string DefaultPathName = @"Z:\Matalino Design\Projects\Duco Development\LoopDrawings\acadtesting\";
+        private const string TITPathName = DefaultPathName + @"\Loop Drawing Typical - TIT.dwg";
 
         public void OpenDrawingReadBlocks(TextBox txtBlockList, TextBox txtAttributeList)
         {
@@ -156,7 +103,7 @@ namespace LoopDrawingAcadUI
         public void OpenTemplatePopulateBlock()
         {
             string dwgFlpath = TITPathName;
-            string savePath = DefaultPathName;
+            string savePath = DefaultPathName + @"testing_loop_attribute_updates.dwg";
 
             using (Database db = new Database(false, true))
             {
@@ -175,7 +122,9 @@ namespace LoopDrawingAcadUI
                             foreach (ObjectId attributeId in ac)
                             {
                                 AttributeReference ar = tr.GetObject(attributeId, OpenMode.ForWrite) as AttributeReference; // get first attribute reference
-                                ar.TextString = TagDict[ar.Tag];
+                                string value = "";
+                                _ = TestData.TagDict.TryGetValue(ar.Tag, out value);
+                                ar.TextString = value;
                             }
                         }
                     }
---
+++ LoopDrawingAcadUI/TestData.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopDrawingAcadUI
+{
+    internal class TestData
+    {
+        public static Dictionary<string, string> TagDict { get; } =  new Dictionary<string, string>
+        {
+            {"DESCRIPTION_01", "DESCRIPTION_01"},
+                    {"DESCRIPTION_02", "DESCRIPTION_02"},
+                    {"DESCRIPTION_03", "DESCRIPTION_03"},
+                    {"MANUFACTURER_01", "MANUFACTURER_01"},
+                    {"MODEL_01", "MODEL_01"},
+                    {"RANGE_01", "RANGE_01"},
+                    {"TAG_01", "TAG_01"},
+                    {"TAG_02", "TAG_02"},
+                    {"CON_01", "CON_01"},
+                    {"CON_02", "CON_02"},
+                    {"JB_TAG_01", "JB_TAG_01"},
+                    {"JB_TS_01", "JB_TS_01"},
+                    {"JB_TB_01", "JB_TB_01"},
+                    {"JB_TB_02", "JB_TB_02"},
+                    {"JB_TB_03", "JB_TB_03"},
+                    {"PNL_TS_01", "PNL_TS_01"},
+                    {"PNL_TB_01", "PNL_TB_01"},
+                    {"PNL_TB_02", "PNL_TB_02"},
+                    {"PNL_TB_03", "PNL_TB_03"},
+                    {"WIRE_CLR_01", "WIRE_CLR_01"},
+                    {"WIRE_CLR_02", "WIRE_CLR_02"},
+                    {"WIRE_CLR_03", "WIRE_CLR_03"},
+                    {"WIRE_CLR_04", "WIRE_CLR_04"},
+                    {"WIRE_CLR_05", "WIRE_CLR_05"},
+                    {"WIRE_CLR_06", "WIRE_CLR_06"},
+                    {"WIRE_CLR_07", "WIRE_CLR_07"},
+                    {"WIRE_CLR_08", "WIRE_CLR_08"},
+                    {"PAIR_01", "PAIR_01"},
+                    {"PAIR_02", "PAIR_02"},
+                    {"WIRE_TAG_01", "WIRE_TAG_01"},
+                    {"WIRE_TAG_02", "WIRE_TAG_02"},
+                    {"WIRE_TAG_03", "WIRE_TAG_03"},
+                    {"CABLE_TAG_01", "CABLE_TAG_01"},
+                    {"CABLE_TAG_02", "CABLE_TAG_02"},
+                    {"BREAKER_01", "BREAKER_01"},
+                    {"MOD_TERM_01", "MOD_TERM_01"},
+                    {"FUNCTIONAL_ID_01", "FUNCTIONAL_ID_01"},
+                    {"LOOP_NO_01", "LOOP_NO_01"},
+
+                    {"ALARM_01", "ALARM_01"},
+                    {"ALARM_02", "ALARM_02"},
+                    {"ALARM_03", "ALARM_03"},
+                    {"ALARM_04", "ALARM_04"},
+                    {"ALARM_05", "ALARM_05"},
+                    {"ALARM_06", "ALARM_06"},
+
+                    {"RACK_01", "RACK_01"},
+                    {"SLOT_01", "SLOT_01"},
+                    {"CHANNEL_01", "CHANNEL_01"},
+                    {"DRAWING_NO_01", "DRAWING_NO_01"},
+        };
+    }
+}
