d37694d3c474a847bb16e0dfbf43d5445a511cce
Mark Dodd <mdodd@ducodesign.ca>
2023-01-20 16:29:48 +0000

    Updated AcadDrawingData.cs

--- LoopDataAccessLayer/AcadDrawingData.cs
+++ LoopDataAccessLayer/AcadDrawingData.cs
@@ -87,40 +87,4 @@ namespace LoopDataAccessLayer
             throw new NotImplementedException();
         }
     }
-
-    //public class AcadDrawingTemplate
-    //{
-    //    private TemplateConfig template;
-    //    public AcadDrawingTemplate(TemplateConfig template)
-    //    {
-    //        this.template = template;
-    //    }
-    //}
-
-    //public class AI_SINGLE : AcadDrawingTemplate
-    //{
-    //    public Dictionary<string, string> BlockMap { get; set; } = new Dictionary<string, string>();
-        
-    //    public AI_SINGLE(TemplateConfig template) : base(template) { }
-
-    //    private void Map()
-    //    {
-
-    //    }
-
-    //}
-
-    //public class AI_AO_BUTTERFLY
-    //{
-    //    public string AI_Tag { get; }
-    //    public string AO_Tag { get; }
-    //    public string Butterfly_Tag { get; }
-
-    //    public AI_AO_BUTTERFLY(string AI_Tag, string AO_Tag, string Butterfly_Tag)
-    //    {
-    //        this.AI_Tag = AI_Tag;
-    //        this.AO_Tag = AO_Tag;
-    //        this.Butterfly_Tag = Butterfly_Tag;
-    //    }
-    //}
 }
